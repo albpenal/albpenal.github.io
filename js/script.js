677c099e23a50d2f29a9154cf6f2a25098228f10
Alberto Peñalba Martos <albpenal@ucm.es>
2025-03-26 12:50:59 +0100

    primer video y estetica cambiada

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
+
+// {Videogame Developer}
 document.addEventListener("DOMContentLoaded", () => {
     const timeline = gsap.timeline({ repeat: -1 });
     const chars = document.querySelectorAll(".text");
 
-    gsap.set(chars, { color: "#008db8" });
+    gsap.set(chars, { color: "#6F0F07" });
 
     timeline.from(chars, { opacity: 1, scale: 0, ease: "sine", delay: 0.25 })
         .to(chars, { 
@@ -16,3 +18,16 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         }, 1);
 });
+
+// FUNCIONES VENTANA EMERGENTE
+function openModal(title, description, videoSrc) {
+    document.getElementById("modal-title").innerText = title;
+    document.getElementById("modal-description").innerHTML = description;
+    document.getElementById("modal-video").querySelector("source").src = videoSrc;
+    document.getElementById("modal-video").load(); // Recargar el video
+    document.getElementById("modal").style.display = "flex";
+}
+function closeModal() {
+    document.getElementById('modal').style.display = 'none';
+    document.getElementById("modal-video").pause();
+}
